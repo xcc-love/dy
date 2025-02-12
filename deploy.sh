781bb52a43c9eac7021233eef066d3db76a375c9
xcc <867747941@qq.com>
2025-02-12 17:04:22 +0800

    自动化部署

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#自动化提交
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd dist
+
+# 初始化 Git 仓库
+echo "Initializing a new Git repository..."
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.gitee.io/<REPO>
+echo "Pushing to GitHub..."
+git push -f git@github.com:xcc-love/dy.git master:main
+
+echo "Deployment complete!"
+cd -
